a388f47ba2242fc89084ea8f2ea217e741cc240c
hikaru.kawauchi <hikaru.kawauchi@gmail.com>
2019-11-14 07:48:34 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_words = user.words.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
